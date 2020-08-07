d3ec42a49f2f25be6364a06bbc05d89c26b6a785
naga1 <naga1@LAPTOP-ICLT5HHI>
2020-08-07 19:36:50 +0530

    Hello

---
+++ Actitme/src/test/java/actitime/Demo.java
@@ -0,0 +1,19 @@
+package actitime;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+
+public class Demo {
+	
+
+		@Test
+		public void demo()
+		{
+			Reporter.log("hello", true);
+			Reporter.log("hello", true);
+			Reporter.log("hello", true);
+		}
+	}
+
+
